f5fe71a5928682e65347093ad5586b921060d32f
okitan <okitakunio@gmail.com>
2023-08-27 22:36:40 +0900

    :bulb: duplicate paren

--- tests/objects/general.test.ts
+++ tests/objects/general.test.ts
@@ -9,9 +9,11 @@ describe("general expression", () => {
   });
 
   describe("parenthesis", () => {
-    test.each([1])("can be parsed from (%s)", (s) => {
-      const literal = `(${s})`;
-      expect(parse(literal)).toMatchObject({ type: "Number", literal, value: s });
+    test.each([
+      ["(1)", 1],
+      ["((1))", 1],
+    ])("can be parsed from %s", (literal, value) => {
+      expect(parse(literal)).toMatchObject({ type: "Number", literal, value });
     });
 
     test.each(["(", ")", "()", "( )"])("considers invalid against %s", (s) => {
