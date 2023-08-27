f851d7c42c3bfc4c91db3917ae0277310a9c3a85
okitan <okitakunio@gmail.com>
2023-08-27 17:16:18 +0900

    :bulb:

--- tests/objects/general.test.ts
+++ tests/objects/general.test.ts
@@ -1,13 +1,20 @@
 import { parse } from "../../src";
 
 describe("general expression", () => {
+  describe("space", () => {
+    // TODO: can be parsed in the future
+    test.each(["1 ", " 1", " 1 "])("considers invalid against '%s'", (s) => {
+      expect(() => parse(s)).toThrowError();
+    });
+  });
+
   describe("parenthesis", () => {
     test.each([1])("can be parsed from (%s)", (s) => {
       const literal = `(${s})`;
       expect(parse(literal)).toMatchObject({ type: "Number", literal, value: s });
     });
 
-    test.each(["(", ")", "()"])("considers invalid against %s", (s) => {
+    test.each(["(", ")", "()", "( )"])("considers invalid against %s", (s) => {
       expect(() => parse(s)).toThrowError();
     });
   });
