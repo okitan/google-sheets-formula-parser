03e86740b192fb1f0a3d815d432e0d8d2df746e0
okitan <okitakunio@gmail.com>
2023-10-07 18:02:16 +0900

    :bulb: prettier

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,43 @@ export function parse(str: string) {
   return parsed;
 }
 
-export function format(parsed: ParseResult): string {
+type Options = {
+  width?: number;
+  insideMatrix?: boolean;
+};
+
+export function format(parsed: ParseResult, options?: Options): string {
+  const { width, insideMatrix } = { width: 80, ...options };
+
+  const remained = width - 2;
+  const optionsForChildren = { ...options, width: remained };
+
   switch (parsed.type) {
+    case "Number":
+      return parsed.value.toString();
+    case "String":
+      return `"${parsed.value}"`;
+    case "Boolean":
+      return parsed.value ? "TRUE" : "FALSE";
+    case "Notation":
+      // TODO:
+      return parsed.literal;
+    case "Symbol":
+      return parsed.value;
     case "UnaryExpression":
+      // no wrap here
       switch (parsed.operator) {
+        case "=":
+          const hasCalculativeOperatorOnLeft =
+            parsed.left.type === "UnaryExpression" && ["+", "-", "*", "/"].includes(parsed.left.operator);
+          const hasCalcurativeOperatorOnRight =
+            parsed.right.type === "UnaryExpression" && ["+", "-", "*", "/"].includes(parsed.right.operator);
+          return [
+            hasCalculativeOperatorOnLeft ? `(${format(parsed.left)})` : format(parsed.left),
+            parsed.operator,
+            hasCalcurativeOperatorOnRight ? `(${format(parsed.right)})` : format(parsed.right),
+          ].join(" ");
+
         case "*":
         case "/":
           const hasAdditiveOperatorOnLeft =
@@ -27,25 +60,42 @@ export function format(parsed: ParseResult): string {
             hasAdditiveOperatorOnRight ? `(${format(parsed.right)})` : format(parsed.right),
           ].join(" ");
         default:
-          return [format(parsed.left), parsed.operator, format(parsed.right)].join(" ");
+          return [
+            format(parsed.left, optionsForChildren),
+            parsed.operator,
+            format(parsed.right, optionsForChildren),
+          ].join(" ");
       }
-    case "Number":
-      return parsed.value.toString();
-    case "String":
-      return `"${parsed.value}"`;
-    case "Boolean":
-      return parsed.value ? "TRUE" : "FALSE";
-    case "Notation":
-      // TODO:
-      return parsed.literal;
-    case "Symbol":
-      return parsed.value;
     case "Function":
-      return [parsed.name.toUpperCase(), "(", parsed.args.map((arg) => (arg ? format(arg) : "")).join(", "), ")"].join(
-        ""
-      );
+      const formattedArgs = parsed.args.map((arg) => (arg ? format(arg, optionsForChildren) : ""));
+
+      const result = parsed.name.toUpperCase() + "(" + formattedArgs.join(", ") + ")";
+      if (result.length <= width) return result;
+
+      const lines: string[] = [formattedArgs[0]];
+      formattedArgs.slice(1).forEach((arg) => {
+        if (lines[lines.length - 1].length + arg.length < remained) {
+          lines[lines.length - 1] = lines[lines.length - 1] + ", " + arg;
+        } else {
+          lines[lines.length - 1] = lines[lines.length - 1] + ",";
+          lines.push(arg);
+        }
+      });
+
+      return [parsed.name.toUpperCase() + "(", indent(lines), ")"].join("\n");
     case "Matrix":
-      return ["{", parsed.values.map((row) => row.map((e) => format(e)).join(", ")).join("; "), "}"].join("");
+      const rows = parsed.values.map((row) => row.map((e) => format(e, { ...optionsForChildren, insideMatrix: true })));
+
+      if (insideMatrix) {
+        if (rows.length === 0) return "";
+        if (rows.length === 1) return rows[0].join(", ");
+      }
+
+      const totalLength = rows.reduce((acc, row) => acc + row.join(", ").length, 0);
+
+      if (totalLength <= width) return "{" + rows.map((row) => row.join(", ")).join("; ") + "}";
+
+      return "{\n  " + rows.map((row) => row.join(", ")).join("\n; ") + "\n}";
   }
 }
 
@@ -55,4 +105,12 @@ function assertParsed(parsed: any): asserts parsed is ParseResult {
   const types = ["UnaryExpression", "Number", "String", "Boolean", "Notation", "Symbol", "Function", "Matrix"];
 
   if (!types.includes(parsed.type)) throw new Error(`parse result is not one of ${types} but ${parsed.type}`);
+}
+
+function indent(lines: string[]): string {
+  return lines
+    .flatMap((line) => line.split("\n"))
+    .filter((e) => e.trim().length)
+    .map((line) => "  " + line)
+    .join("\n");
 }
--- tests/formatter.test.ts
+++ tests/formatter.test.ts
@@ -13,6 +13,8 @@ describe(format, () => {
     ["-1--2", "-1 - -2"],
     // String Operator
     [`"hoge"&"fuga"`, `"hoge" & "fuga"`],
+    // Comparison
+    ["(1+2)=(3*1)", "(1 + 2) = (3 * 1)"],
     // Boolean
     ["true", "TRUE"],
     ["false", "FALSE"],
@@ -23,8 +25,72 @@ describe(format, () => {
     // Function
     ["sum(1,2)", "SUM(1, 2)"],
     // Matrix
+    ["{}", "{}"],
+    ["{{}}", "{}"],
+    ["{1,2}", "{1, 2}"],
+    ["{{1,2}}", "{1, 2}"],
+    ["{1;2}", "{1; 2}"],
     ["{1,2;3,4}", "{1, 2; 3, 4}"],
+    ["{{1,2};{3,4}}", "{1, 2; 3, 4}"],
   ])(`should format %s to %s`, (input, expected) => {
     expect(format(parse(input))).toBe(expected);
+  });
+
+  test.each([
+    [
+      "2(indent) + 3(digit comma space) * 3 - 1 = 10",
+      "SUM(1,2,3,4,5)",
+      10,
+      `SUM(
+  1, 2, 3,
+  4, 5
+)`,
+    ],
+    [
+      "2(indent) + 3(digit comma space) * 3 - 2 = 9",
+      "SUM(1,2,3)",
+      9,
+      `SUM(
+  1, 2, 3
+)`,
+    ],
+    [
+      "`SUM(1, 2)` is 9 chars, 9 < 11 - 2 measn it can be in one line",
+      "IF(SUM(1,2), SUM(1,2),)",
+      11,
+      `IF(
+  SUM(1, 2),
+  SUM(1, 2),
+)`,
+    ],
+    [
+      "multiple if",
+      "IF(IF(IF(1=1, TRUE, FALSE),),)",
+      10,
+      `IF(
+  IF(
+    IF(
+      1 = 1,
+      TRUE,
+      FALSE
+    ),
+  ),
+)`,
+    ],
+    [
+      "matrix should be wrapped",
+      "{ 1, 2, 3; 4, 5, 6}",
+      10,
+      `{
+  1, 2, 3
+; 4, 5, 6
+}`,
+    ],
+  ])("in case of %s, should wrap %s into %i chars", (_name, input, width, expected) => {
+    expect(format(parse(input), { width })).toBe(expected);
+
+    // property based test
+    const lines = format(parse(input)).split("\n");
+    expect(Math.max(...lines.map((e) => e.length))).toBeLessThanOrEqual(80);
   });
 });
